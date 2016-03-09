bce716324e4db9937739f94d1888365639e37774
marklin <marklin@nine-yi.com>
2016-03-09 15:55:36 +0800

    TDD HW 修正為有意義的命名

--- HW1_SpecByTest/UnitTest1.cs
+++ HW1_SpecByTest/UnitTest1.cs
@@ -12,19 +12,19 @@ namespace TestSpec
         {
             ///arrange
             var target = new ProductionCode();            
-            var stubData = new List<StubData>
+            var stubData = new List<StubProduct>
             {
-                new StubData { Id = 1 , Cost = 1 ,Revenue =11 ,SellPrice =21 },
-                new StubData { Id = 2 , Cost = 2 ,Revenue =12 ,SellPrice =22 },
-                new StubData { Id = 3 , Cost = 3 ,Revenue =13 ,SellPrice =23 },
-                new StubData { Id = 4 , Cost = 4 ,Revenue =14 ,SellPrice =24 },
-                new StubData { Id = 5 , Cost = 5 ,Revenue =15 ,SellPrice =25 },
-                new StubData { Id = 6 , Cost = 6 ,Revenue =16 ,SellPrice =26 },
-                new StubData { Id = 7 , Cost = 7 ,Revenue =17 ,SellPrice =27 },
-                new StubData { Id = 8 , Cost = 8 ,Revenue =18 ,SellPrice =28 },
-                new StubData { Id = 9 , Cost = 9 ,Revenue =19 ,SellPrice =29 },
-                new StubData { Id = 10 , Cost = 10 ,Revenue =20 ,SellPrice =30 },
-                new StubData { Id = 11 , Cost = 11 ,Revenue =21 ,SellPrice =31 },
+                new StubProduct { Id = 1 , Cost = 1 ,Revenue =11 ,SellPrice =21 },
+                new StubProduct { Id = 2 , Cost = 2 ,Revenue =12 ,SellPrice =22 },
+                new StubProduct { Id = 3 , Cost = 3 ,Revenue =13 ,SellPrice =23 },
+                new StubProduct { Id = 4 , Cost = 4 ,Revenue =14 ,SellPrice =24 },
+                new StubProduct { Id = 5 , Cost = 5 ,Revenue =15 ,SellPrice =25 },
+                new StubProduct { Id = 6 , Cost = 6 ,Revenue =16 ,SellPrice =26 },
+                new StubProduct { Id = 7 , Cost = 7 ,Revenue =17 ,SellPrice =27 },
+                new StubProduct { Id = 8 , Cost = 8 ,Revenue =18 ,SellPrice =28 },
+                new StubProduct { Id = 9 , Cost = 9 ,Revenue =19 ,SellPrice =29 },
+                new StubProduct { Id = 10 , Cost = 10 ,Revenue =20 ,SellPrice =30 },
+                new StubProduct { Id = 11 , Cost = 11 ,Revenue =21 ,SellPrice =31 },
             };
             var propertyName = "Cost";
             var sumOfNum = 3;
@@ -40,19 +40,19 @@ namespace TestSpec
         {
             ///arrange
             var target = new ProductionCode();
-            var stubData = new List<StubData>
+            var stubData = new List<StubProduct>
             {
-                new StubData { Id = 1 , Cost = 1 ,Revenue =11 ,SellPrice =21 },
-                new StubData { Id = 2 , Cost = 2 ,Revenue =12 ,SellPrice =22 },
-                new StubData { Id = 3 , Cost = 3 ,Revenue =13 ,SellPrice =23 },
-                new StubData { Id = 4 , Cost = 4 ,Revenue =14 ,SellPrice =24 },
-                new StubData { Id = 5 , Cost = 5 ,Revenue =15 ,SellPrice =25 },
-                new StubData { Id = 6 , Cost = 6 ,Revenue =16 ,SellPrice =26 },
-                new StubData { Id = 7 , Cost = 7 ,Revenue =17 ,SellPrice =27 },
-                new StubData { Id = 8 , Cost = 8 ,Revenue =18 ,SellPrice =28 },
-                new StubData { Id = 9 , Cost = 9 ,Revenue =19 ,SellPrice =29 },
-                new StubData { Id = 10 , Cost = 10 ,Revenue =20 ,SellPrice =30 },
-                new StubData { Id = 11 , Cost = 11 ,Revenue =21 ,SellPrice =31 },
+                new StubProduct { Id = 1 , Cost = 1 ,Revenue =11 ,SellPrice =21 },
+                new StubProduct { Id = 2 , Cost = 2 ,Revenue =12 ,SellPrice =22 },
+                new StubProduct { Id = 3 , Cost = 3 ,Revenue =13 ,SellPrice =23 },
+                new StubProduct { Id = 4 , Cost = 4 ,Revenue =14 ,SellPrice =24 },
+                new StubProduct { Id = 5 , Cost = 5 ,Revenue =15 ,SellPrice =25 },
+                new StubProduct { Id = 6 , Cost = 6 ,Revenue =16 ,SellPrice =26 },
+                new StubProduct { Id = 7 , Cost = 7 ,Revenue =17 ,SellPrice =27 },
+                new StubProduct { Id = 8 , Cost = 8 ,Revenue =18 ,SellPrice =28 },
+                new StubProduct { Id = 9 , Cost = 9 ,Revenue =19 ,SellPrice =29 },
+                new StubProduct { Id = 10 , Cost = 10 ,Revenue =20 ,SellPrice =30 },
+                new StubProduct { Id = 11 , Cost = 11 ,Revenue =21 ,SellPrice =31 },
             };
             var propertyName = "Revenue";
             var sumOfNum = 4;
@@ -64,7 +64,7 @@ namespace TestSpec
         }
     }
 
-    internal class StubData
+    internal class StubProduct
     {
         public int Id { get; set; }
         public int Cost { get; set; }
